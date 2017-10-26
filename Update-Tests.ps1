97c797d2030f133a95393f3343e3e7beed3a8f39
Matthew Long <matthew.long@squaredup.com>
2017-10-26 15:17:35 +0100

    updated comment based help

--- Update-Tests.ps1
+++ Update-Tests.ps1
@@ -1,6 +1,12 @@
 <#
 .SYNOPSIS
     Deploys the Pack.tests.template.ps1 file out to each pack folder, overwriting the contents.  This is to ensure that all packs have up to date tests prior to running invoke-pester.
+
+.PARAMETER Path
+    The location to (recursivly) start searching for packs that need updated tests.
+
+.PARAMETER TemplateFile
+    Path to the template file that you want to update the tests with.
 #>
 
 [CmdletBinding()]
